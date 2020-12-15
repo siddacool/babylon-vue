04018a606885f5f4ba9a3700b5147ee571429a15
Siddhesh Mangela <sidthecool007@gmail.com>
2020-12-15 15:46:04 +0530

    ball added

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -18,18 +18,15 @@ let engine: any = null;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const createScene = (engine: any, canvas: any) => {
-  // Create a basic BJS Scene object
-  var scene = new Scene(engine);
-  // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
-  var camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene);
-  // Target the camera to scene origin
+  const scene = new Scene(engine);
+  const camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene);
+
   camera.setTarget(Vector3.Zero());
-  // Attach the camera to the canvas
   camera.attachControl(canvas, false);
-  // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
-  var light = new HemisphericLight('light1', new Vector3(0, 1, 0), scene);
-  // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
-  var sphere = Mesh.CreateSphere(
+
+  const light = new HemisphericLight('light1', new Vector3(0, 4, 0), scene);
+
+  const sphere = Mesh.CreateSphere(
     'sphere1',
     16,
     2,
@@ -37,11 +34,11 @@ const createScene = (engine: any, canvas: any) => {
     false,
     Mesh.FRONTSIDE,
   );
-  // Move the sphere upward 1/2 of its height
+
   sphere.position.y = 1;
-  // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
-  var ground = Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
-  // Return the created scene
+
+  // const ground = Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
+
   return scene;
 };
 
