84590a99150abc270073f829082787a484dc6c2f
Siddhesh Mangela <sidthecool007@gmail.com>
2020-12-15 14:52:00 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+html {
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+}
+
+body {
+  min-height: 100vh;
+}
+
+#app {
+  min-height: inherit;
+}
+</style>
---
+++ src/view/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Babylon Vue</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+});
+</script>
+
+<style lang="scss">
+.home {
+  min-height: inherit;
+}
+</style>
