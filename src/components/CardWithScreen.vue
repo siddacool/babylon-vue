5e36cc528c75a1326fc09ad61d20a1064bc18149
Siddhesh Mangela <sidthecool007@gmail.com>
2020-12-17 16:02:19 +0530

    card component separated

---
+++ src/components/CardWithScreen.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="flex flex-col h-40 border-2 border-blue-800 p-4 rounded mb-4 md:mb-0"
+  >
+    <div class="h-full mb-2 bg-blue-600"></div>
+    <div class="h-10 text-sm">{{ name }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CardWithScreen',
+  props: {
+    name: String,
+  },
+});
+</script>
+
+<style lang="scss"></style>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -9,12 +9,7 @@
           :to="`/games/${route.id}`"
           class="block"
         >
-          <div
-            class="flex flex-col h-40 border-2 border-blue-800 p-4 rounded mb-4 md:mb-0"
-          >
-            <div class="h-full mb-2 bg-blue-600"></div>
-            <div class="h-10 text-sm">{{ route.name }}</div>
-          </div>
+          <CardWithScreen :name="route.name" />
         </router-link>
       </div>
     </div>
@@ -24,10 +19,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import gamesRoutes from '../router/games-routes';
+import CardWithScreen from '../components/CardWithScreen.vue';
 
 export default defineComponent({
   name: 'HomeView',
-  setup() {
+  components: {
+    CardWithScreen,
+  },
+  data() {
     return {
       gamesRoutes,
     };
