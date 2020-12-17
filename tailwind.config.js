27254c0051ef2a6454ba6c7181a0bac4648ed8ae
Siddhesh Mangela <sidthecool007@gmail.com>
2020-12-17 10:30:33 +0530

    primary color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
       // eslint-disable-next-line quotes
       sans: `'Rubik', sans-serif`,
     },
+    colors: {
+      primary: 'var(--c-colors-primary)',
+    },
   },
   variants: {},
   plugins: [],
