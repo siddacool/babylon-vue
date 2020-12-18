852fd3f6d71a020809ebc8dbb7d6579b41658c3e
Siddhesh Mangela <sidthecool007@gmail.com>
2020-12-18 11:51:52 +0530

    components removed from game routes

--- src/router/games-routes.ts
+++ src/router/games-routes.ts
@@ -2,12 +2,10 @@ const gamesRoutes = [
   {
     id: 'game-s1',
     name: 'Game S1',
-    component: import('../games/GameS1.vue'),
   },
   {
     id: 'game-s2',
     name: 'Game S2',
-    component: import('../games/GameS2.vue'),
   },
 ];
 
